9758f2a7c4773f47fb373b67d570541c19ba1713
Javad Malmir <17jm17@gmail.com>
2025-07-26 16:31:42 +0330

    add handlers

--- HR_Management.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeListRequest.cs
+++ HR_Management.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeListRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using HR_Management.Application.DTOs.LeaveType;
 using MediatR;
 
-namespace HR_Management.Application.Features.LeaveTypes.Requests
+namespace HR_Management.Application.Features.LeaveTypes.Requests.Queries
 {
     public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
     {
