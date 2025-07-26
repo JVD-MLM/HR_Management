b93230971dbb82143b3de956986ac5251e1246aa
Javad Malmir <17jm17@gmail.com>
2025-07-26 13:39:03 +0330

    add application layer and generic repository

---
+++ HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Persistence.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<T> Get(int id);
+        public Task<IReadOnlyList<T>> GetAll();
+        public Task<T> Add(T entity);
+        public Task<T> Update(T entity);
+        public Task<T> Delete(T entity);
+    }
+}
---
+++ HR_Management.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR_Management.Domain;
+
+namespace HR_Management.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
