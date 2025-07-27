abf8514a453929315b4221622aabf9fdfd3ec8f7
Javad Malmir <17jm17@gmail.com>
2025-07-27 09:14:33 +0330

    add delete commands

---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypesCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR_Management.Application.Persistence.Contracts;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypesCommandHandler : IRequestHandler<DeleteLeaveTypesCommand, Unit>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypesCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypesCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            await _leaveTypeRepository.Update(leaveType);
+            return Unit.Value;
+        }
+    }
+}
---
+++ HR_Management.Application/Features/LeaveTypes/Requests/Commands/DeleteLeaveTypesCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HR_Management.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class DeleteLeaveTypesCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
