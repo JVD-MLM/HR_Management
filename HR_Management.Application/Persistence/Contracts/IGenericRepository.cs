8d4d68ae0ac776e1399925bc8ea9b14961145d3d
Javad Malmir <17jm17@gmail.com>
2025-07-27 13:07:11 +0330

    implement generic repository methods

--- HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
@@ -5,7 +5,7 @@
         public Task<T> Get(int id);
         public Task<IReadOnlyList<T>> GetAll();
         public Task<T> Add(T entity);
-        public Task<T> Update(T entity);
-        public Task<T> Delete(T entity);
+        public Task Update(T entity);
+        public Task Delete(T entity);
     }
 }
